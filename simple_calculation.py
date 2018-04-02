88c38b294413f274e2db7f00d60f4170d641e5f3
IceCreammm <contactme897@gmail.com>
2018-04-02 00:31:53 -0400

    simple _calculation
    to do some simple calculation to get some numbers.

---
+++ simple_calculation.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sector_dir = {'sector':['Consumer Discretionary', 'Consumer Staples', 'Utilities', 
+                        'Technology', 'Health Care', 'Financial', 'Energy' , 'Telecom', 
+                        'Industrials', 'Material', 'Real Estate'],'Market_Cap':[5.59, 3.58, 1.24, 9.04, 4.96, 7.67, 
+                                                                3.69, 1.69,4.21, 2.14, 1.13]}
+sector_importance = pd.DataFrame(data=sector_dir)
+
+
+print(sector_importance)
+
+sector_importance['weight'] = sector_importance['Market_Cap']/sector_importance['Market_Cap'].sum()
+
+sector_importance.plot(kind='hist')
+plt.show()
+
+
+
+
+
+
+
+
